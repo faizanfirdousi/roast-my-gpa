960862b57c8ca7c4d577e6b3d3c55a3a79c70757
faizanfirdousi <faizanfirdousi.official@gmail.com>
2025-07-12 10:30:03 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RoastMyGPA from './page.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RoastMyGPA />
+    <Analytics />
   </StrictMode>,
 )
